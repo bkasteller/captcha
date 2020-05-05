5b8f2394a323e4ce233f3f528819ab5af502e793
bkasteller <43139731+bkasteller@users.noreply.github.com>
2020-05-05 18:43:19 +0200

    Récupération du fichier json
    installation de body-parser

--- CaptCha/serveur.js
+++ CaptCha/serveur.js
@@ -1,13 +1,19 @@
 let express = require('express')
-
+let fs = require('fs')
 let app = express()
+let bodyParser = require('body-parser')
 
+// Moteur de template
 app.set('view engine', 'ejs')
 
-app.use(express.static('images'))
+// Middleware
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
+// Routes
 app.get('/', (request, response) => {
-  response.render('captcha')
+  response.render('captcha', {data: JSON.parse(fs.readFileSync('public/images/LesChats.json', 'utf8'))})
 })
 
 app.listen(8080)
